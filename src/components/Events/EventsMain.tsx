c6e6a1858e3a873f638f1a500ac4f144f9b05d80
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-07-23 15:52:13 +0200

    simplifying events main component

--- src/components/Events/EventsMain.tsx
+++ src/components/Events/EventsMain.tsx
@@ -1,37 +1,11 @@
-import ClayLoadingIndicator from '@clayui/loading-indicator'
-
-import { useQuery } from '@tanstack/react-query'
-import fetchEvents from 'api/fetchEvents'
-
-import EventGrid from './EventGrid'
+import PastEvents from './PastEvents'
 
 const EventsMain = () => {
-  const {
-    data = {},
-    error,
-    isError,
-    isLoading,
-  } = useQuery(['events'], () => fetchEvents({ start: 0 }))
-
-  const { pastEvents, upcomingEvents } = data
-
   return (
     <section>
-      {isLoading ? (
-        <ClayLoadingIndicator displayType="secondary" size="sm" />
-      ) : (
-        <>
-          <EventGrid featureFirst events={upcomingEvents} />
-          <EventGrid events={pastEvents} />
-        </>
-      )}
+      <PastEvents />
     </section>
   )
 }
 
 export default EventsMain
-
-interface EventsData {
-  pastEvents: Events
-  upcomingEvents: Events
-}
