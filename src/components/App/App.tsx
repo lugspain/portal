96591d6349bb1c18fc9f0820ff763a8eabdff1f8
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-07-23 15:53:18 +0200

    update configs for react query

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import '@clayui/css/lib/css/atlas.css'
 
@@ -6,13 +7,20 @@ import Footer from 'components/layout/Footer/Footer'
 import Header from 'components/layout/Header/Header'
 import Main from 'components/layout/Main/Main'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <Header />
     <Main />
     <Footer />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 )
 
