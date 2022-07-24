ef62d465ab9357deb61038a19901872c7bb65eaa
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-07-24 22:54:22 +0200

    button copy

--- src/components/Events/LoadMoreButton/LoadMoreButton.tsx
+++ src/components/Events/LoadMoreButton/LoadMoreButton.tsx
@@ -19,7 +19,7 @@ const LoadMoreButton = ({
             <ClayLoadingIndicator displayType="secondary" size="sm" light />
           </span>
         )}
-        {hasNextPage ? 'Ver más' : 'No hay más'}
+        {hasNextPage ? 'Ver más' : 'No hay más eventos :('}
       </ClayButton>
     </LoadMoreButtonStyled>
   )
