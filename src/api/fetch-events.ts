9f327c58d32620fcdea3d0241e2ca09fbd390a53
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-07-24 15:33:44 +0200

    fix endpoint url

---
+++ src/api/fetch-events.ts
@@ -0,0 +1,24 @@
+import http from './http'
+
+const fetchEvents = async ({
+  pageParam = 1,
+  upcomingEvents = false,
+}: {
+  pageParam?: number
+  upcomingEvents?: boolean
+}) => {
+  try {
+    const { data } = await http.get(`/api/events`, {
+      params: {
+        page: pageParam,
+        upcomingEvents,
+      },
+    })
+    return data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export default fetchEvents
