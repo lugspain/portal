6f046654674d1a0ce8f6cadc07d4de38bd97bf01
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-07-26 16:35:32 +0200

    feedback box

--- src/components/UI/Feedback/Feedback.tsx
+++ src/components/UI/Feedback/Feedback.tsx
@@ -1,5 +1,36 @@
+import ClayLink from '@clayui/link'
+import feedbackImage from 'assets/images/feedback.jpg'
+import {
+  FeedbackStyled,
+  FeedbackContentStyled,
+  FeedbacktitleStyled,
+} from './FeedbackStyled'
+
 const Feedback = () => {
-  return <p>feedback module</p>
+  return (
+    <FeedbackStyled>
+      <figure>
+        <img src={feedbackImage} alt="Dar feedback" />
+      </figure>
+      <FeedbackContentStyled>
+        <FeedbacktitleStyled>¿Tienes alguna sugerencia?</FeedbacktitleStyled>
+        <p>
+          Si quieres hacernos llegar alguna sugerencia o quieres proponer algún
+          tema a tratar, nos encantará recibirlas.
+        </p>
+        <p>
+          <ClayLink href="https://forms.gle/zJa1NMce2Jgh8HaZA" target="_blank">
+            Buzón de sugerencias
+          </ClayLink>
+        </p>
+        <p>
+          <ClayLink href="https://forms.gle/3MDtQAV1z3xnLhtN8" target="_blank">
+            Propuesta de charla
+          </ClayLink>
+        </p>
+      </FeedbackContentStyled>
+    </FeedbackStyled>
+  )
 }
 
 export default Feedback
---
+++ src/components/UI/Feedback/FeedbackStyled.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const FeedbackStyled = styled.article`
+  border-radius: 4px;
+  overflow: hidden;
+`
+
+export const FeedbackContentStyled = styled.div`
+  padding: 12px;
+`
+
+export const FeedbacktitleStyled = styled.h3`
+  margin-bottom: 12px;
+`
