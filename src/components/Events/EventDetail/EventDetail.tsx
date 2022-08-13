78b6a200ee0b7678bbc683c1464a1ae7ec03af3f
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-08-13 15:03:13 +0200

    Add iframe for youtube video and get youtube link from from string

--- src/components/Events/EventDetail/EventDetail.tsx
+++ src/components/Events/EventDetail/EventDetail.tsx
@@ -2,10 +2,22 @@ import { useQuery } from '@tanstack/react-query'
 import fetchEvent from 'api/fetch-event'
 const EventDetail = ({ eventId }: IProps) => {
   const eventDetail = useQuery(['event'], () => fetchEvent({ id: eventId }))
+  const targetString: string =
+    'Podréis asistir en directo por streaming en [youtube](https://www.youtube.com/watch?v=8BGfDJ1j5-Q).\n\nEn la [página del evento ](https://lugspain.github.io/mesdeqa/) podéis consultar más información sobre las sesiones, las comunidades y los ponentes que participan.'
+  const rExp: RegExp = /https:\/\/www\.youtube\.com\/watch\?v=.{11}/
+  const result: RegExpMatchArray | null = targetString.match(rExp)
 
   return (
     <>
       <p>{eventId}</p>
+      <p>{result}</p>
+      <iframe
+        title="myFrame"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/watch?v=JnjxpmPjRTw"
+        frameBorder="0"
+      ></iframe>
       <pre>{JSON.stringify(eventDetail, undefined, 2)}</pre>
     </>
   )
