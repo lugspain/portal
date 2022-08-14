eefedf849fb7cc55b0be31403a8bcce8c4ff577a
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-08-14 16:24:52 +0200

    Read status from query to know if data is already available and replace string in youtube link so it works correctly for the iframe

--- src/components/Events/EventDetail/EventDetail.tsx
+++ src/components/Events/EventDetail/EventDetail.tsx
@@ -1,24 +1,40 @@
+import ClayLoadingIndicator from '@clayui/loading-indicator'
 import { useQuery } from '@tanstack/react-query'
 import fetchEvent from 'api/fetch-event'
+import { ClayLoadingIndicatorWrapperStyled } from 'assets/styles/containers'
 const EventDetail = ({ eventId }: IProps) => {
-  const eventDetail = useQuery(['event'], () => fetchEvent({ id: eventId }))
-  const targetString: string =
-    'Podréis asistir en directo por streaming en [youtube](https://www.youtube.com/watch?v=8BGfDJ1j5-Q).\n\nEn la [página del evento ](https://lugspain.github.io/mesdeqa/) podéis consultar más información sobre las sesiones, las comunidades y los ponentes que participan.'
-  const rExp: RegExp = /https:\/\/www\.youtube\.com\/watch\?v=.{11}/
-  const result: RegExpMatchArray | null = targetString.match(rExp)
+  const { status, data } = useQuery(['event'], () =>
+    fetchEvent({ id: eventId })
+  )
+
+  if (data) {
+    const rExp: RegExp = /https:\/\/www\.youtube\.com\/watch\?v=.{11}/
+    const result: RegExpMatchArray | null = data.description.match(rExp)
+    var resultString: string | undefined = result?.pop()
+    var replace = /watch\?v=/gi
+    resultString = resultString?.replace(replace, 'embed/')
+  }
 
   return (
     <>
-      <p>{eventId}</p>
-      <p>{result}</p>
-      <iframe
-        title="myFrame"
-        width="560"
-        height="315"
-        src="https://www.youtube.com/watch?v=JnjxpmPjRTw"
-        frameBorder="0"
-      ></iframe>
-      <pre>{JSON.stringify(eventDetail, undefined, 2)}</pre>
+      {status === 'loading' ? (
+        <ClayLoadingIndicatorWrapperStyled>
+          <ClayLoadingIndicator displayType="secondary" size="lg" />
+        </ClayLoadingIndicatorWrapperStyled>
+      ) : (
+        <>
+          <p>{eventId}</p>
+          <p>{resultString}</p>
+          <iframe
+            title="myFrame"
+            width="560"
+            height="315"
+            src={resultString}
+            frameBorder="0"
+          ></iframe>
+          <pre>{JSON.stringify(data.description, undefined, 2)}</pre>
+        </>
+      )}
     </>
   )
 }
