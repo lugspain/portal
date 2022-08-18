aae63f3aa95e7ade64fd8af1d0359f5f5defce02
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-08-18 13:10:04 +0200

    Handle different forms of Youtube links and get video Id in all the cases

--- src/components/Events/MeetupYoutubeVideo/MeetupYoutubeVideo.tsx
+++ src/components/Events/MeetupYoutubeVideo/MeetupYoutubeVideo.tsx
@@ -6,7 +6,37 @@ const MeetupYoutubeVideo = ({ description }: IProps) => {
   const endOfYoutubeLink = arrayDescription[1].indexOf(')')
   const youtubeLink = arrayDescription[1].slice(1, endOfYoutubeLink)
 
-  const mockedYoutubeLink = 'https://www.youtube.com/embed/8BGfDJ1j5-Q'
+  const rExp: RegExp = /https?:\/\/www\.youtube\.com/
+  const rExp1: RegExp = /https?:\/\/www\.youtube\.com\/embed/
+  const isCompleteURL: boolean = rExp.test(youtubeLink)
+  const hasEmbed: boolean = rExp1.test(youtubeLink)
+
+  var ampersandPosition: number = 0
+  var indexOfCharacterV: number = 0
+  var videoId: string
+  if (!isCompleteURL) {
+    videoId = youtubeLink.slice(16, youtubeLink.length)
+    videoId = 'https://www.youtube.com/embed/' + videoId
+  } else if (hasEmbed) {
+    videoId = youtubeLink
+  } else {
+    var arr_video_id = youtubeLink.split('v=')
+    if (arr_video_id.length < 2) {
+      indexOfCharacterV = arr_video_id[0].indexOf('v')
+      videoId = arr_video_id[0].slice(
+        indexOfCharacterV + 2,
+        indexOfCharacterV + 13
+      )
+    } else {
+      ampersandPosition = arr_video_id[1].indexOf('&')
+      if (ampersandPosition !== -1) {
+        videoId = arr_video_id[1].substring(0, ampersandPosition)
+      } else {
+        videoId = arr_video_id[1].substring(0, 11)
+      }
+    }
+    videoId = 'https://www.youtube.com/embed/' + videoId
+  }
 
   return (
     <>
@@ -15,7 +45,7 @@ const MeetupYoutubeVideo = ({ description }: IProps) => {
         title="myFrame"
         width="560"
         height="315"
-        src={mockedYoutubeLink}
+        src={videoId}
         frameBorder="0"
       ></iframe>
     </>
