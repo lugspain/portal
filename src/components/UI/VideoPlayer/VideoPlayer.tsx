be3d98ea671693cedf5cacb4301bf0b1ca0e46f6
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-09-27 21:22:11 +0200

    fix: get exact video id

--- src/components/UI/VideoPlayer/VideoPlayer.tsx
+++ src/components/UI/VideoPlayer/VideoPlayer.tsx
@@ -1,15 +1,28 @@
+import { IframeWrapperStyled } from './VideoPlayerStyled'
+
+const YOUTUBE_EMBED_BASE_URL = 'https://www.youtube.com/embed'
+const YOUTUBE_LINK_REGEX =
+  /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/gi
+const YOUTUBE_ID_REGEX = /([A-Za-z0-9_\-]{11})/gi
+
+const _getYoutubeVideoId = (string: string): string => {
+  const _link: RegExpMatchArray | '' | null = string.match(YOUTUBE_LINK_REGEX)
+  const id: RegExpMatchArray | '' | null = _link?.length
+    ? _link[0].match(YOUTUBE_ID_REGEX)
+    : ''
+
+  return id && id.length ? id[0] : ''
+}
+
 const useYoutubeEmbedUrl = (text: string | undefined): string | undefined => {
   if (!text) {
     return
   }
-  const YOUTUBE_EMBED_BASE_URL = 'http://www.youtube.com/embed/'
 
-  const youtubeLink = text.match(
-    /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi
-  )
+  const youtubeVideoId = _getYoutubeVideoId(text)
 
-  return youtubeLink
-    ? YOUTUBE_EMBED_BASE_URL + youtubeLink[0].split('v=')[1]
+  return youtubeVideoId
+    ? `${YOUTUBE_EMBED_BASE_URL}/${youtubeVideoId}`
     : undefined
 }
 
