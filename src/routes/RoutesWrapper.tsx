e10d1adce1235ae115ba8ee0dbf07da26c586f20
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-10-17 15:31:08 +0200

    feat: add PageNotFound barebones

--- src/routes/RoutesWrapper.tsx
+++ src/routes/RoutesWrapper.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from 'routes/Home/Home'
 import Auth from 'routes/Auth/Auth'
 import Event from 'routes/Event/Event'
+import PageNotFound from 'routes/PageNotFound/PageNotFound'
 
 const RoutesWrapper = () => {
   return (
@@ -9,6 +10,7 @@ const RoutesWrapper = () => {
       <Route path="/" element={<Home />} />
       <Route path="auth" element={<Auth />} />
       <Route path="event/:event" element={<Event />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   )
 }
