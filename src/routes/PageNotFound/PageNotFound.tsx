fc32df950b03959676ddfba8b30eac07021f8e77
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-10-17 16:54:18 +0200

    feat: 404 page, version 1

--- src/routes/PageNotFound/PageNotFound.tsx
+++ src/routes/PageNotFound/PageNotFound.tsx
@@ -1,15 +1,33 @@
 import ClayAlert from '@clayui/alert'
-import { ContentContainer } from 'assets/styles/containers'
-import styled from 'styled-components'
+import { ContentContainer, GridContainer } from 'assets/styles/containers'
+import { useNavigate } from 'react-router-dom'
+import ClayButton from '@clayui/button'
+import { HistoryLocation } from 'types'
 
-const PageNotFound = () => {
+const PageNotFound = ({ location }: IProps) => {
+  const { pathname } = location
+  const navigate = useNavigate()
+
   return (
     <div>
-      <ContentContainer useScreenMinHeight>
-        <ClayAlert>Página no encontrada</ClayAlert>
+      <ContentContainer padY={80} useScreenMinHeight>
+        <h1>Error 404</h1>
+        <ClayAlert displayType="danger" variant="feedback">
+          Página no encontrada
+        </ClayAlert>
+        <p>
+          La ruta <code>`{pathname}`</code> no está disponible.
+        </p>
+        <div>
+          <ClayButton onClick={() => navigate('/')}>Home</ClayButton>
+        </div>
       </ContentContainer>
     </div>
   )
+}
+
+interface IProps {
+  location: HistoryLocation
 }
 
 export default PageNotFound
--- src/routes/RoutesWrapper.tsx
+++ src/routes/RoutesWrapper.tsx
@@ -3,14 +3,18 @@ import Home from 'routes/Home/Home'
 import Auth from 'routes/Auth/Auth'
 import Event from 'routes/Event/Event'
 import PageNotFound from 'routes/PageNotFound/PageNotFound'
+import { useLocation } from 'react-router-dom'
+import { HistoryLocation } from 'types'
 
 const RoutesWrapper = () => {
+  const location: HistoryLocation = useLocation()
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="auth" element={<Auth />} />
       <Route path="event/:event" element={<Event />} />
-      <Route path="*" element={<PageNotFound />} />
+      <Route path="*" element={<PageNotFound location={location} />} />
     </Routes>
   )
 }
