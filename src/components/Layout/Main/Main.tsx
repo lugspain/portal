18df746e5ba764849a45ea839fbb21f1b68fe7c2
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-10-17 12:55:06 +0200

    feat: add auth route. Wrap all routes in an independent component

--- src/components/Layout/Main/Main.tsx
+++ src/components/Layout/Main/Main.tsx
@@ -1,14 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from 'routes/Home/Home'
-import Event from 'routes/Event/Event'
+import RoutesWrapper from 'routes/RoutesWrapper'
 import { MainStyled } from './MainStyled'
 
 const Main = () => (
   <MainStyled>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="event/:event" element={<Event />} />
-    </Routes>
+    <RoutesWrapper />
   </MainStyled>
 )
 
---
+++ src/routes/Auth/Auth.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from 'react-router-dom'
+
+const Auth = () => {
+  return <Navigate to="/" replace={true} />
+}
+
+export default Auth
