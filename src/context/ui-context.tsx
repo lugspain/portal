a0ec05fad7759bf937be03fe947e3be85e48d61f
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-10-18 09:13:20 +0200

    feat: new ui context

---
+++ src/context/ui-context.tsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useReducer } from 'react'
+
+const initialValue: any = {}
+
+const UiContext = createContext(initialValue)
+UiContext.displayName = 'UiContext'
+
+const uiReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'HIDE_SEARCH': {
+      return {
+        ...state,
+        showSearch: false,
+      }
+    }
+    case 'SHOW_SEARCH': {
+      return {
+        ...state,
+        showSearch: true,
+      }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+const UiProvider = ({ children }: { children: JSX.Element }) => {
+  const [state, dispatch] = useReducer(uiReducer, initialValue)
+
+  return (
+    <UiContext.Provider value={{ state, dispatch }}>
+      {children}
+    </UiContext.Provider>
+  )
+}
+
+const useUiContext = () => {
+  const context = useContext(UiContext)
+
+  if (!Object.keys(context).length) {
+    throw new Error(`useUiContext must be used within a UiProvider`)
+  }
+
+  return context
+}
+
+export { UiProvider, useUiContext }
