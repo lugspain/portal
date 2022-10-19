e775768037204d50e612be715f53258ad5ef57d7
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-10-19 09:17:41 +0200

    feat: hero section responsiveness

--- src/components/UI/Hero/HeroStyled.tsx
+++ src/components/UI/Hero/HeroStyled.tsx
@@ -26,10 +26,43 @@ export const HeroStyled = styled.section`
 
 export const HeroContent = styled.div`
   position: absolute;
-  top: 50%;
-  left: 15%;
+  top: 25%;
+  left: 2%;
   color: white;
   background-color: rgb(0 0 0 / 60%);
-  padding: 32px;
+  padding: 12px;
   border-radius: 12px;
+
+  h1 {
+    font-size: 1.2rem;
+  }
+  p {
+    font-size: 0.9rem;
+  }
+
+  @media ${({ theme }) => theme.devices.mobileL} {
+    h1 {
+      font-size: 1.4rem;
+    }
+    p {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${({ theme }) => theme.devices.tablet} {
+    top: 35%;
+    left: 10%;
+    padding: 24px;
+    font-size: 95%;
+
+    h1 {
+      font-size: 1.625rem;
+    }
+  }
+
+  @media ${({ theme }) => theme.devices.laptop} {
+    top: 50%;
+    padding: 32px;
+    font-size: 100%;
+  }
 `
