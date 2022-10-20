cec80f7a9a34100c31c04bbcf8f57a8b1d9da159
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-10-20 17:12:12 +0200

    test: adding more test for homepage

--- cypress/e2e/home-past-events.cy.ts
+++ cypress/e2e/home-past-events.cy.ts
@@ -1,13 +1,42 @@
 /// <reference types="cypress" />
 
 describe('Past events section in home...', () => {
-  it('...has proper title', () => {
+  before(() => {
     cy.visit('/')
+  })
 
+  it('...has proper title', () => {
     cy.get('[data-cy="past-events-title"]').contains('Eventos anteriores')
   })
 
-  it('...loads the default amount of events', () => {
+  it('...loads the proper amount of events', () => {
     cy.get('[data-cy="past-event-item-grid"]').should('have.length', 6)
+
+    cy.contains('Cargar más eventos').click()
+
+    cy.get('[data-cy="past-event-item-grid"]').should('have.length', 12)
+
+    cy.contains('Cargar más eventos').click()
+
+    cy.get('[data-cy="past-event-item-grid"]').should('have.length', 18)
+  })
+
+  it.only('...allows to navigate to each event', () => {
+    cy.get('[data-cy="past-event-item-grid"]')
+      .first()
+      .find('a')
+      .first()
+      .should('have.attr', 'href')
+      .and('include', 'event/')
+
+    cy.get('[data-cy="past-event-item-grid"]')
+      .first()
+      .find('a')
+      .first()
+      .invoke('attr', 'href')
+      .then((href) => {
+        cy.visit(href!)
+        cy.url().should('include', href)
+      })
   })
 })
