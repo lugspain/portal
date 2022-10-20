f15aa21aab6871ed46048bf901c58b39a788eb44
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-10-20 18:01:07 +0200

    chore: cleanup

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   e2e: {
     projectId: 'pgr5kd',
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 })
